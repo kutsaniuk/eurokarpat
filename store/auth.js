5366ce3056a2a1d01216416734d2e846bd86284f
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 15:07:58 +0300

    Add CORS

--- store/auth.js
+++ store/auth.js
@@ -13,6 +13,7 @@ export const actions = {
   async login({commit}, data) {
     return this.$axios.$post('/auth/login', data)
       .then(async (res) => {
+        console.log(res)
         commit('setUser', res.data.user);
       })
   },
