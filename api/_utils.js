e143869f82f387bb86ce5827a348c23d4877f20e
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 16:44:19 +0300

    Add CORS

--- api/_utils.js
+++ api/_utils.js
@@ -26,7 +26,7 @@ export const handler = async (req, res) => {
       res.json(response.data);
     }
   } catch (error) {
-    return res.json(JSON.parse(JSON.stringify(req)));
+    return res.json({s: req.path});
     // return res.status(error.response.status).json({ type: 'error', message: error.response.data.message });
   }
 }
