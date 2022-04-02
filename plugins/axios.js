151af3b682bf0731138a93c99902d2e639738533
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2022-04-02 17:25:04 +0300

    Add Posts

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,8 @@
 export default function ({ $axios }) {
   $axios.onError((error) => {
     switch (error.response && error.response.status) {
-      case 401: location.replace('/login'); break
-      case 403: location.replace('/login'); break
+      // case 401: location.replace('/login'); break
+      // case 403: location.replace('/login'); break
     }
   })
 }
